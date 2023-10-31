dce4ddecd6c5c834ca6900680dc33fb57a2b4b81
Dawid Ciepiela <71898979-sarumaj@users.noreply.github.com>
2023-11-01 00:06:38 +0100

    clean up

--- cmd/ldap/main.go
+++ cmd/ldap/main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"os"
-	"runtime/pprof"
-
-	supererrors "github.com/sarumaj/go-super/errors"
 	commands "github.com/sarumaj/ldap-cli/pkg/app/commands"
 	apputil "github.com/sarumaj/ldap-cli/pkg/app/util"
 )
@@ -19,12 +15,6 @@ var BuildDate = "0000-00-00 00:00:00 UTC"
 
 func main() {
 	apputil.Logger.Debugf("version: %q, build date: %q", Version, BuildDate)
-
-	f := supererrors.ExceptFn(supererrors.W(os.OpenFile("profile.prof", os.O_CREATE|os.O_TRUNC|os.O_RDWR, os.ModePerm)))
-	defer f.Close()
-
-	supererrors.Except(pprof.StartCPUProfile(f))
-	defer pprof.StopCPUProfile()
 
 	commands.Execute(Version, BuildDate)
 }
