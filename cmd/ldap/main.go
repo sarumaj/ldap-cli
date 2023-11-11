1fe5e71b2e31c5ea8f30ea4df2b4ad612bb3bc21
Dawid Ciepiela <71898979-sarumaj@users.noreply.github.com>
2023-11-11 13:29:07 +0100

    command search custom/user

--- cmd/ldap/main.go
+++ cmd/ldap/main.go
@@ -1,6 +1,9 @@
 package main
 
-import commands "github.com/sarumaj/ldap-cli/pkg/app/commands"
+import (
+	commands "github.com/sarumaj/ldap-cli/pkg/app/commands"
+	apputil "github.com/sarumaj/ldap-cli/pkg/app/util"
+)
 
 // Version holds the application version.
 // It gets filled automatically at build time.
@@ -11,5 +14,6 @@ var Version = "v0.0.0"
 var BuildDate = "0000-00-00 00:00:00 UTC"
 
 func main() {
+	apputil.Logger.Debugf("version: %q, build date: %q", Version, BuildDate)
 	commands.Execute(Version, BuildDate)
 }
--- pkg/lib/definitions/attributes/type_attribute_map.go
+++ pkg/lib/definitions/attributes/type_attribute_map.go
@@ -1,49 +1,14 @@
 package attributes
 
-import (
-	"bytes"
-	"fmt"
-
-	yaml "gopkg.in/yaml.v3"
-)
-
 type Map map[Attribute]any
 
-func (r Map) String() string {
-	m := make(map[string]any)
-	for a, v := range r {
-		m[a.String()] = v
+func (r Map) Keys() (keys Attributes) {
+	for a := range r {
+		keys = append(keys, a)
 	}
 
-	buffer := bytes.NewBuffer(nil)
-	enc := yaml.NewEncoder(buffer)
-	enc.SetIndent(2)
-	if err := enc.Encode(m); err != nil {
-		return fmt.Sprint(m)
-	}
-
-	return buffer.String()
+	keys.Sort()
+	return
 }
 
 type Maps []map[Attribute]any
-
-func (rs Maps) String() string {
-	var maps []map[string]any
-	for _, r := range rs {
-		m := make(map[string]any)
-		for a, v := range r {
-			m[a.String()] = v
-		}
-
-		maps = append(maps, m)
-	}
-
-	buffer := bytes.NewBuffer(nil)
-	enc := yaml.NewEncoder(buffer)
-	enc.SetIndent(2)
-	if err := enc.Encode(maps); err != nil {
-		return fmt.Sprint(maps)
-	}
-
-	return buffer.String()
-}
