2960bc3f64aa021422d69e8dca2d521ca3f2e8b5
Dawid Ciepiela <71898979-sarumaj@users.noreply.github.com>
2023-11-23 13:29:07 +0100

    command search

--- cmd/ldap/main.go
+++ cmd/ldap/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import commands "github.com/sarumaj/ldap-cli/pkg/app/commands"
 
+// Version holds the application version.
+// It gets filled automatically at build time.
+var Version = "v0.0.0"
+
+// BuildDate holds the date and time at which the application was build.
+// It gets filled automatically at build time.
+var BuildDate = "0000-00-00 00:00:00 UTC"
+
+func main() {
+	commands.Execute(Version, BuildDate)
 }
