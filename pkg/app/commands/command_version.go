899a3059cfb66e7a3c94939dd053724255c43a1f
Dawid Ciepiela <71898979-sarumaj@users.noreply.github.com>
2024-01-15 23:04:29 +0000

    fix version cmd

--- pkg/app/commands/command_version.go
+++ pkg/app/commands/command_version.go
@@ -75,7 +75,6 @@ func versionRun(*cobra.Command, []string) {
 
 	}
 
-	fmt.Println("test")
 	_ = supererrors.ExceptFn(supererrors.W(fmt.Fprintln(apputil.Stdout(), apputil.PrintColors(color.CyanString, "Version: %s", internalVersion+vSuffix))))
 	_ = supererrors.ExceptFn(supererrors.W(fmt.Fprintln(apputil.Stdout(), apputil.PrintColors(color.CyanString, "Built at: %s", internalBuildDate))))
 	_ = supererrors.ExceptFn(supererrors.W(fmt.Fprintln(apputil.Stdout(), apputil.PrintColors(color.CyanString, "Executable path: %s", libutil.GetExecutablePath()))))
