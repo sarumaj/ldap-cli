af36854eca8f87d225c0d6cad4ab05021cb0ad2c
Dawid Ciepiela <71898979-sarumaj@users.noreply.github.com>
2024-01-17 19:30:03 +0000

    configure keyring backends

--- pkg/lib/util/keyring.go
+++ pkg/lib/util/keyring.go
@@ -1,14 +1,31 @@
 package util
 
 import (
+	"fmt"
+
 	keyring "github.com/99designs/keyring"
+	survey "github.com/AlecAivazis/survey/v2"
 )
 
-const Service = "ldap-cli"
-
 var Config = keyring.Config{
-	ServiceName:     Service,
-	AllowedBackends: keyring.AvailableBackends(),
+	AllowedBackends:                keyring.AvailableBackends(),
+	FileDir:                        "~/.config/ldap-cli",
+	FilePasswordFunc:               passwordFunc,
+	KeyCtlScope:                    "user",
+	KeyCtlPerm:                     keyring.KEYCTL_PERM_USER,
+	KeychainAccessibleWhenUnlocked: true,
+	KeychainName:                   "ldap-cli",
+	KeychainPasswordFunc:           passwordFunc,
+	KeychainSynchronizable:         false,
+	KeychainTrustApplication:       true,
+	LibSecretCollectionName:        "ldap-cli",
+	KWalletAppID:                   "ldap-cli",
+	KWalletFolder:                  "ldap-cli",
+	PassCmd:                        "pass",
+	PassDir:                        "~/.config/.ldap-cli",
+	PassPrefix:                     "ldap-cli.",
+	ServiceName:                    "ldap-cli",
+	WinCredPrefix:                  "ldap-cli.",
 }
 
 func GetFromKeyring(key string) (string, error) {
@@ -23,6 +40,23 @@ func GetFromKeyring(key string) (string, error) {
 	}
 
 	return string(item.Data), nil
+}
+
+func passwordFunc(s string) (string, error) {
+	var got string
+	err := survey.AskOne(&survey.Password{Message: s}, &got, survey.WithValidator(survey.ComposeValidators(survey.Required, func(ans interface{}) error {
+		if str, ok := ans.(string); !ok || len(str) < 12 {
+			return fmt.Errorf("password must be at least 12 characters long")
+		}
+
+		return nil
+	})))
+
+	if err != nil {
+		return "", err
+	}
+
+	return got, nil
 }
 
 func RemoveFromKeyRing(key string) error {
